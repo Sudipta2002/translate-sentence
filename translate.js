f0844630ffc99a273a1425742b0ccbb3eb2375c5
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2022-01-11 19:37:04 +0530

    API updated
    error handeller added

--- translate.js
+++ translate.js
@@ -1,11 +1,20 @@
 var inputText = document.querySelector('#input-text');
 var outputText = document.querySelector('#output-text');
 var buttonTranslate = document.querySelector('#btn-translate');
+var apiUrl = "https://api.funtranslations.com/translate/minion.json";
 
-
+function errorHandle(error) {
+    alert('Error occured')
+    console.log("error occured", error);
+}
 
 function clickHandler() {
-    console.log("clicked");
-    console.log(inputText.value);
+    var text = inputText.value;
+    var updatedUrl = apiUrl + "?text=" + text;
+    fetch(updatedUrl).then(response => response.json()).then(json => outputText.innerText = (json.contents.translated)).catch(errorHandle);
+    // outputText.innerText = json.
 }
+
+
+
 buttonTranslate.addEventListener("click", clickHandler);
